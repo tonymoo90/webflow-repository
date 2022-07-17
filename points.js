02228bd17a1eb3d4692f7468dd4a9fb790dc9bce
JJ D'Aurora <jjdaurora@JJs-MacBook-Pro.local>
2022-07-17 23:18:40 +0800

    points.js; y/n styles

--- points.js
+++ points.js
@@ -49,14 +49,27 @@ fetch('https://api.airtable.com/v0/appzvtjAGocJzOExM/Member.Bets?filterByFormula
             let balance = document.createElement("div")
             balance.setAttribute("class", "table-data")
 
+            if (f.fields['Closing Date'] && f.fields.Category === "winning")  {
+              tableBoxBalance.setAttribute("style",'text-align: center;justify-content: center;align-items: center;background:#00b300;font-weight: bold;width: 5%; ')
+            }
+
+            else if (f.fields['Closing Date'] && f.fields.Category !== "winning")  {
+              tableBoxBalance.setAttribute("style",'text-align: center;justify-content: center;align-items: center;background:#FF0000;font-weight: bold;width: 5%; ')
+            }
+
+            else if (!f.fields['Closing Date']) {
+              tableBoxBalance.setAttribute("style",'text-align: center;justify-content: center;align-items: center;background:#EBEBE4;font-weight: bold;width: 5%; ')
+              
+            }
+
             if (f.fields.choice && (f.fields.choice === true || f.fields.choice === "true")) {
-              tableBoxBalance.setAttribute("style",'text-align: center;justify-content: center;align-items: center;background: #EBEBE4;font-weight: bold;')
               balance.innerHTML = 'YES' 
             }
+            
             else {
-              tableBoxBalance.setAttribute("style",'text-align: center;justify-content: center;align-items: center;background: #EBEBE4;font-weight: bold;')
               balance.innerHTML = ' NO' 
             }
+
             tableBoxBalance.appendChild(balance)
 
             let tableBoxPending = document.createElement("div")
