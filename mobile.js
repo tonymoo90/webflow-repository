af5936fad9b40a447fd6f63822d4bcc88591c73d
tonymoo90 <tonymoo90@gmail.com>
2022-07-21 21:47:57 -0500

    Update mobile.js
    this is the time elapsed javascript code, not sure if inserted correctly

--- mobile.js
+++ mobile.js
@@ -132,3 +132,66 @@ $(".trigger").each(function (index) {
     duration: 0.3
   });
 });
+
+// Time Elapsed
+  function timePast(curr, prev) {
+    //define the milliseconds in every time unit
+    var msMin = 60 * 1000;
+    var msHr = msMin * 60;
+    var msDay = msHr * 24;
+    var msMonth = msDay * 30;
+    var msYr = msDay * 365;
+
+    //get elapsed time in milliseconds
+    var elapsed = curr - prev;
+
+    if (elapsed < msMin) {
+      return Math.round(elapsed/1000) + ' seconds ago';
+    }
+    else if (elapsed < msHr) {
+      var elapsed = Math.round(elapsed/msMin);
+      if (elapsed === 1) {        // Show singular or plural depending on return value
+        return elapsed + ' minute ago';
+      } else {
+        return elapsed + ' minutes ago';
+      }
+    }
+    else if (elapsed < msDay) {
+      var elapsed = Math.round(elapsed/msHr);
+      if (elapsed === 1) { 
+        return elapsed + ' hour ago';
+      } else {
+        return elapsed + ' hours ago';
+      }
+    }
+    else if (elapsed < msMonth) {
+      var elapsed = Math.round(elapsed/msDay);
+      if (elapsed === 1) {
+        return elapsed + ' day ago';
+      } else {
+        return elapsed + ' days ago';
+      }   
+    }
+    else if (elapsed < msYr) {
+      var elapsed = Math.round(elapsed/msMonth);
+      if (elapsed === 1) {
+        return elapsed + ' month ago';
+      } else {
+        return elapsed + ' months ago';
+      } 
+    }
+    else {
+      var elapsed = Math.round(elapsed/msYr);
+      if (elapsed === 1) {
+        return elapsed + ' year ago';
+      } else {
+        return elapsed + ' years ago';
+      }
+    }
+  }
+
+  $('.post-date-5, .post-date-4,.post-date-3,.post-date-2,.post-date').each(function() { //replace '.post-date' with your date's class
+    var now = new Date();
+    var parsedTime = Date.parse($(this).text());
+    $(this).text(timePast(now, new Date(parsedTime)));
+  });
